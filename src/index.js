4dd596b97c261538d51afab636328306bf9324d9
VyacheslavRus <78310890+VyacheslavRus@users.noreply.github.com>
2021-04-02 16:01:19 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './styles.css';
+import './js/food-menu.js'
+import './js/them-swicher.js'
+import './css/styles.css';
---
+++ src/js/food-menu.js
@@ -0,0 +1,7 @@
+import menuTemplate from '../templates/menu.hbs'
+import menuData from '../menu.json'
+
+const foodMenu = menuTemplate(menuData)
+
+const renderMenu = document.querySelector('.js-menu')
+renderMenu.insertAdjacentHTML('beforeend', renderMenu)
